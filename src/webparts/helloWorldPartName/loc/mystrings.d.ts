3e95115c4de202420bcd4a60080ba38005d4a22f
JonasKaruzas <jonas.karuzas@gmail.com>
2023-12-09 14:47:08 +0200

    initial

---
+++ src/webparts/helloWorldPartName/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IHelloWorldPartNameWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'HelloWorldPartNameWebPartStrings' {
+  const strings: IHelloWorldPartNameWebPartStrings;
+  export = strings;
+}
